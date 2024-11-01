bfb0a8e1fa317ad9da9d7c59af6be472c456f317
Luccon <lucas.barboza405@gmail.com>
2024-11-01 18:43:19 -0300

    Estudo JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.write("<h1>Hello World</h1>");
+let eu  = {
+    nome : "Lucas",
+    idade : 24,
+    genero : "masculino",
+    habilidades : ["HTML","CSS","JavaScript","C"]
+}
+
+function mudarcor(cor){
+    
+    document.body.style.backgroundColor = cor;
+}
+function mudarcor(){
+    document.body.style.backgroundColor = document.getElementById('sel').value;
+}
